e3e21594f93738f6e6a1e1d61c8572de8404555c
Esk <Esk@Esk-PC>
2014-04-13 12:18:21 -0400

    New Commit

--- public_html/controllers/pageprofile.php
+++ public_html/controllers/pageprofile.php
@@ -1,6 +1,7 @@
 <?php
 class pageprofile extends controller{
     
+    
     public function index($pageID)
     {
         //parent::__construct($registry);
@@ -23,7 +24,20 @@ class pageprofile extends controller{
         $this->data['profile_email']= $profile[0]['email'];
         $this->data['profile_picture']=  "../image/default-avatar.png";
         $this->data['profile_about'] = $profile[0]['about'];
-        $this->data['profile_menu']= array('Profile','Info','Friends','Timeline');
+        $this->data['profile_userid']=$profile[0]['UserId'];
+        $this->data['friends']=array();
+        require_once 'system/library/timeconvert.php';
+        
+        $converter = new timeconvert();
+        $datetime = $converter->convert_datetime($profile[0]['time']);
+        $this->data['profile_datejoined']=  $converter->makeAgo($datetime);
+        
+        $this->data['profile_menu']= array(
+            'Profile'=>array('href'=>$this->data['profile_username'].'/profile','title'=>'Profile'),
+            'Info'=>array('href'=>$this->data['profile_username'].'/info','title'=>'Info'),
+            'Friends'=>array('href'=>$this->data['profile_username'].'/friends','title'=>'Friends'),
+            'Timeline'=>array('href'=>$this->data['profile_username'].'/timeline','title'=>'Timeline')
+            );
         
         $this->render();
         
@@ -36,11 +50,19 @@ class pageprofile extends controller{
        // }
         
     }
+
     
-    public function addfriend($friendid)
+    public function friends()
     {
         
- 
+        $this->load->model('user');
+        $friendslist = $this->model_user->getFriendsList($this->data['profile_userid']);
+        $this->data['friends'] = $friendslist;
+          foreach ($friendslist as $key => $value) {
+              
+            echo $friendslist[$key]['username']."<br>" ;
+          }   
+          //$this->render();
     }
     
     
